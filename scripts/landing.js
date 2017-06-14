b46e2682db56206fa0237055e2844e15815ef3a4
Vincent Scotti <scottivincentj@gmail.com>
2017-06-13 20:53:14 -0400

    Checkpoint 9: This looks mumuch cleaner then my recent submission.

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,12 +1,28 @@
-var points = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName('point');
 
-var revealPoints = function(index) {
-  for(var i=0; i < index.length; i++) {
-    index[i].style.opacity = 1;
-    index[i].style.transform = "scaleX(1) translateY(0)";
-    index[i].style.msTransform = "scaleX(1) translateY(0)";
-    index[i].style.webkitTransform = "scaleX(1) translateY(0)";
-  };
+var animatePoints = function(pointsArray) {
+    for(var i=0; i < pointsArray.length; i++) {
+      pointsArray[i].style.opacity = 1;
+      pointsArray[i].style.transform = "scaleX(1) translateY(0)";
+      pointsArray[i].style.msTransform = "scaleX(1) translateY(0)";
+      pointsArray[i].style.webkitTransform = "scaleX(1) translateY(0)";
+    }; //close for
+}; //close ap
+
+
+//window. can be set to function to perform action
+window.onload = function() {
+
+  if (window.innerHeight > 950) {
+    animatePoints(pointsArray);
+  }
+
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+  window.addEventListener("scroll", function(event) {
+    if(document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+      animatePoints(pointsArray);
+    }
+  });
 };
-
-revealPoints(points);
