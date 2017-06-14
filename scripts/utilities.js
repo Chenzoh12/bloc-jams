e7a5f886b0ad7501b56c00ce73336cbd0bed8550
Vincent Scotti <scottivincentj@gmail.com>
2017-06-14 18:42:36 -0400

    I could not think of anythign clever for the callback functions. Could you give me some examples of the practical or common uses?

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -1,4 +1,16 @@
 
-window.onload = function() {
 
-}; //
+  var pointsArray = document.getElementsByClassName('point');
+
+  function logIt(callback) {
+    callback();
+  }
+
+  function testConsole() {
+  //Insert functionality here
+    console.log("Test: " + i)
+  }
+
+  for(var i=0; i<pointsArray.length; i++) {
+    logIt(testConsole);
+  }
